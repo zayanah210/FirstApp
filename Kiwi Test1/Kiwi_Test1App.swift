5ac1b605fc2604a728786be2ff3baf0cccd1391f
Tuhin Mahmud <tuhinm@us.ibm.com>
2023-03-24 18:04:11 -0500

    Initial Commit

---
+++ Kiwi Test1/Kiwi_Test1App.swift
@@ -0,0 +1,17 @@
+//
+//  Kiwi_Test1App.swift
+//  Kiwi Test1
+//
+//  Created by Tuhin Mahmud on 3/24/23.
+//
+
+import SwiftUI
+
+@main
+struct Kiwi_Test1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
